ce7a1ac6b6e7dc2f2ad973f359b7317bdfc5892c
Josh Cheek <josh.cheek@gmail.com>
2015-09-13 18:55:27 -0600

    Hearts for all the humans!

--- examples/print_username.rb
+++ examples/print_username.rb
@@ -10,7 +10,19 @@ class User
   def name=(name)
     @name = name
   end
+
+  def location
+    "the book club!"
+  end
+
+  def friends
+    "Austin and Lovisa!"
+  end
 end
 
 user = User.new("Josh")
 puts user.name
+puts "was at"
+puts user.location
+puts "with"
+puts user.friends
